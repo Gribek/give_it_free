edb0e3f9a8ddca4d92468d009796a1aae6713f67
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-01-21 20:21:53 +0100

    Prepared static and templates folders, updated settings.py

--- give_it_free/settings.py
+++ give_it_free/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'give_it_free.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["GiveItFreeApp/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
